187a929164ff8d2bd5a64752d58d07334f6b19b0
nullchinchilla <nullchinchilla@pm.me>
2023-02-21 17:18:07 -0600

    oops

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 mod txbuilder;
+use std::str::FromStr;
+
+use thiserror::Error;
 use tmelcrypt::HashVal;
 pub use txbuilder::*;
 mod units;
@@ -19,4 +22,31 @@ pub use header::*;
 pub struct Checkpoint {
     pub height: BlockHeight,
     pub header_hash: HashVal,
+}
+
+#[derive(Error, Debug)]
+pub enum ParseCheckpointError {
+    #[error("expected a ':' character to split the height and header hash")]
+    ParseSplitError,
+    #[error("height is not an integer")]
+    ParseIntError(#[from] std::num::ParseIntError),
+    #[error("failed to parse header hash as a hash")]
+    ParseHeaderHash(#[from] hex::FromHexError),
+}
+
+impl FromStr for Checkpoint {
+    type Err = ParseCheckpointError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (height_str, hash_str) = s
+            .split_once(':')
+            .ok_or(ParseCheckpointError::ParseSplitError)?;
+
+        let height = BlockHeight::from_str(height_str)?;
+        let header_hash = HashVal::from_str(hash_str)?;
+
+        Ok(Self {
+            height,
+            header_hash,
+        })
+    }
 }
